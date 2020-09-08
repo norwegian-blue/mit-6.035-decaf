7f8e2bdfeade379355338ff43971689a8b4c4ca9
Nicola Pompini <nicola.pompini@gmail.com>
2020-09-08 10:04:27 +0200

    Fixed test script for antlr4 parser

--- skeleton/test/parser/test.sh
+++ skeleton/test/parser/test.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 runparser() {
-  $(git rev-parse --show-toplevel)/skeleton/run.sh -t parse $1
+  $(git rev-parse --show-toplevel)/skeleton/run.sh -target parse $1
 }
 
 fail=0
 
-for file in `dirname $0`/illegal1; do
-  if runparser > /dev/null 2>&1 $file; then
+for file in `dirname $0`/illegal/*; do
+  runparser > tmp 2>&1 $file
+  if ! [ -s tmp ]; then
     echo "Illegal file $file parsed successfully.";
     fail=1
   fi
+  rm tmp
 done
 
-for file in `dirname $0`/legal0*; do
-  if ! runparser $file; then
+for file in `dirname $0`/legal/*; do
+  runparser 2>&1 $file | tee -a tmp
+  if [ -s tmp ]; then
     echo "Legal file $file failed to parse.";
     fail=1
   fi
+  rm tmp
 done
 
 exit $fail;
