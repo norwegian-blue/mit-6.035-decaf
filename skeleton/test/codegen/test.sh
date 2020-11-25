7413daf266f295b0839d151744e7a3101f464a65
Nicola Pompini <nicola.pompini@gmail.com>
2020-11-26 00:43:34 +0100

    Added test suit for codegen phase

--- skeleton/test/codegen/test.sh
+++ skeleton/test/codegen/test.sh
@@ -1,30 +1,57 @@
 #!/bin/bash
 
-runsemantic() {
-  $(git rev-parse --show-toplevel)/skeleton/run.sh -target inter $1
+runassembler() {
+  $(git rev-parse --show-toplevel)/skeleton/run.sh -target codegen -o $2 $1
 }
 
 fail=0
 
-for file in `dirname $0`/illegal/*; do
-  if runsemantic > /dev/null 2>tmp_err $file; then
-    echo "Illegal file $file passed semantic check.";
-    fail=1
-  else
-    grep "ERROR.*" tmp_err -o > tmp_errAct;
-    grep "ERROR.*" $file -o > tmp_errTgt;
-    if ! diff tmp_errAct tmp_errTgt > /dev/null; then
-      echo "Detected semantic error in $file does not match expectation.";
-    fi
-  fi
-  rm tmp_*;
-done
+for file in `dirname $0`/input/*.dcf; do
+  asm='tmp.s'
+  bin='tmp'
 
-for file in `dirname $0`/legal/*; do
-  if ! runsemantic $file; then
-    echo "Legal file $file failed semantic check.";
-    fail=1
+  # compile to assembly
+  if runassembler $file $asm 2>&1 >/dev/null; then
+    
+    # assemble code and run
+    if gcc -no-pie -o $bin $asm 2>&1 >/dev/null; then
+      output='out';
+      ./$bin > $output;
+      exitcode=$?
+
+      # check return value for error
+      if [ -f "$(dirname $0)"/error/"$(basename $file .dcf)".err ]; then
+	errfile="$(dirname $0)"/error/"$(basename $file .dcf)".err;
+        val=$(<$errfile)
+	if [ $val != $exitcode ]; then
+	  echo "Program $file is expected to return runtime error $val";
+          fail=1;
+        fi
+      else 
+        if [ $exitcode != 0 ]; then
+          echo "Program $file returns an unexpected runtime error";
+          fail=1;
+	fi
+      fi
+
+      # check program output 
+      if  ! diff $output "$(dirname $0)"/output/"$(basename $file .dcf)".out > /dev/null; then
+        echo "Program $file output does not match expected result"
+	fail=1;
+      fi
+
+    else 
+      echo "Program $file failed to assemble";
+      fail=1;
+    fi
+
+  else 
+    echo "Program $file failed to compile";
+    fail=1;    
   fi
+
+  rm $asm $bin $output;
+
 done
 
 exit $fail;
