f39f977585bd696ae92bd97caf8207d1e7a11093
Nicola Pompini <nicola.pompini@gmail.com>
2021-01-02 18:04:34 +0100

    draft optimizer test

---
+++ skeleton/test/optimizer/test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+runassembler() {
+  $(git rev-parse --show-toplevel)/skeleton/run.sh -target codegen -opt all -o $2 -debug $1
+}
+
+fail=0
+
+for file in `dirname $0`/input/*.dcf; do
+  asm='tmp.s'
+  bin='tmp'
+
+  echo "running " $file;
+  # compile to assembly
+  if runassembler $file $asm 2>&1 > /dev/null; then
+    
+    # assemble code and run
+    if gcc -no-pie -o $bin $asm 2>&1 > /dev/null; then
+      output='out';
+      ./$bin > $output;
+      exitcode=$?
+
+      # check return value for error
+      if [ -f "$(dirname $0)"/error/"$(basename $file .dcf)".err ]; then
+	errfile="$(dirname $0)"/error/"$(basename $file .dcf)".err;
+        val=$(<$errfile)
+	if [ $val != $exitcode ]; then
+	  echo "Program $file is expected to return runtime error $val";
+          fail=1;
+        fi
+      else 
+        if [ $exitcode != 0 ]; then
+          echo "Program $file returns an unexpected runtime error";
+          fail=1;
+	fi
+      fi
+
+      # check program output 
+      if  ! diff $output "$(dirname $0)"/output/"$(basename $file .dcf)".out > /dev/null; then
+        echo "Program $file output does not match expected result"
+	fail=1;
+      fi
+
+    else 
+      echo "Program $file failed to assemble";
+      fail=1;
+    fi
+
+  else 
+    echo "Program $file failed to compile";
+    fail=1;    
+  fi
+
+  rm -f $asm $bin $output;
+
+done
+
+exit $fail;
