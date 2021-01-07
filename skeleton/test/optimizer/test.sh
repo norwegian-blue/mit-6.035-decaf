c16c086b7743c581562381d0e6189b6e499e4ddd
Nicola Pompini <nicola.pompini@gmail.com>
2021-01-08 00:51:21 +0100

    Updated testing for all available optimizations (dataflow + register allocation)

--- skeleton/test/optimizer/test.sh
+++ skeleton/test/optimizer/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 runassembler() {
-  $(git rev-parse --show-toplevel)/skeleton/run.sh -target codegen -opt -o $2 -debug $1
+  $(git rev-parse --show-toplevel)/skeleton/run.sh -target codegen -opt all -o $2 -debug $1
 }
 
 fail=0
